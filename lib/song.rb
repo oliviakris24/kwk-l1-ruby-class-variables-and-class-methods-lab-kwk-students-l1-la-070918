b872be1989ef945b87361e6bff5b319cf17186ec
oliviakris24 <okfishy@gmail.com>
2018-07-13 18:34:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,23 +15,23 @@ class Song
    @@artists.uniq
  end
 
- def self.genre_count
-   @@genre_1 = {}
-   @@genres.each do |genre|
-     @@genre_1[genre] ||= 0
-     @@genre_1[genre] += 1
-   end
-   @@genre_1
- end
+# def self.genre_count
+#   @@genre_1 = {}
+#   @@genres.each do |genre|
+#     @@genre_1[genre] ||= 0
+#     @@genre_1[genre] += 1
+#   end
+#   @@genre_1
+# end
 
- def self.artist_count
-   @@artist_1 = {}
-   @@artists.each do |artist|
-     @@artist_1[artist] ||= 0
-     @@artist_1[artist] += 1
-   end
-   @@artist_1
- end
+# def self.artist_count
+#   @@artist_1 = {}
+#   @@artists.each do |artist|
+#     @@artist_1[artist] ||= 0
+#     @@artist_1[artist] += 1
+#   end
+#   @@artist_1
+# end
 
  attr_accessor :name, :artist, :genre
 
